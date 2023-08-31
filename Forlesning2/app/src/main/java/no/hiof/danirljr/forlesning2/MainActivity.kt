90af97addf4071dc1f586a00b339c7fc3b094e44
danieljohnrussell <danieljr@hiof.no>
2023-08-31 10:26:56 +0200

    Color Changeing screen by button
    Dont have nothing to explane:(

--- Forlesning2/app/src/main/java/no/hiof/danirljr/forlesning2/MainActivity.kt
+++ Forlesning2/app/src/main/java/no/hiof/danirljr/forlesning2/MainActivity.kt
@@ -3,12 +3,25 @@ package no.hiof.danirljr.forlesning2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import no.hiof.danirljr.forlesning2.ui.theme.Forlesning2Theme
 
@@ -22,25 +35,79 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ColorChangingScreen()
                 }
             }
         }
     }
 }
+@Composable
+fun ColorChangingButtonOwnState(modifier : Modifier = Modifier) {
+    var backgroundColor by remember {mutableStateOf(Color.White)}
+
+    Box(contentAlignment = Alignment.Center,
+        modifier = modifier
+            .fillMaxSize()
+            .background(backgroundColor)) {
+        Button(
+            onClick = {
+                backgroundColor = if(backgroundColor == Color.White)
+                    Color.Cyan
+                else
+                    Color.White
+            }
+        ) {
+            Text(text = stringResource(R.string.change_color))
+        }
+    }
+}
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun ColorChangingButton(currentColor : Color,
+                        updateColor: (Color) -> Unit,
+                        modifier : Modifier = Modifier) {
+    Box(contentAlignment = Alignment.Center,
         modifier = modifier
-    )
+            .background(currentColor)) {
+        Button(
+            onClick = {
+                val newColor = when (currentColor) {
+                    Color.White -> Color.Cyan
+                    Color.Cyan -> Color.Magenta
+                    else -> Color.White
+                }
+
+                updateColor(newColor)
+            }
+        ) {
+            Text(text = stringResource(R.string.change_color))
+        }
+    }
 }
+
+@Composable
+fun ColorChangingScreen(modifier: Modifier = Modifier) {
+    var backgroundColor1 by remember {mutableStateOf(Color.Cyan)}
+    var backgroundColor2 by remember {mutableStateOf(Color.Cyan)}
+
+    Column(verticalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier.fillMaxSize()) {
+        ColorChangingButton(currentColor = backgroundColor1,
+            updateColor = {newColor -> backgroundColor1 = newColor},
+            Modifier.weight(1.0f).fillMaxWidth())
+
+        ColorChangingButton(currentColor = backgroundColor2,
+            updateColor = {newColor -> backgroundColor2 = newColor},
+            Modifier.weight(1.0f).fillMaxWidth())
+    }
+
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Forlesning2Theme {
-        Greeting("Android")
+        ColorChangingScreen()
     }
 }
