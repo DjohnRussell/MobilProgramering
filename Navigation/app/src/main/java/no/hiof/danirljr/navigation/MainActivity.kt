a40f919770fb842e1b70da2d610802acb8a6b94a
danieljohnrussell <danieljr@hiof.no>
2023-09-11 14:58:49 +0200

    Corrected the structure
    made a folder for the files

--- Navigation/app/src/main/java/no/hiof/danirljr/navigation/MainActivity.kt
+++ Navigation/app/src/main/java/no/hiof/danirljr/navigation/MainActivity.kt
@@ -9,13 +9,9 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
