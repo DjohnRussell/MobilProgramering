2ad8750a33cf4d38f2310cac7457ba89dbfcace8
danieljohnrussell <danieljr@hiof.no>
2023-09-18 14:08:20 +0200

    Changed up the theam

--- MatirialDesign/app/src/main/java/no/hiof/danirljr/matirialdesign/MainActivity.kt
+++ MatirialDesign/app/src/main/java/no/hiof/danirljr/matirialdesign/MainActivity.kt
@@ -1,46 +1,78 @@
 package no.hiof.danirljr.matirialdesign
 
+import android.graphics.Paint.Style
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import no.hiof.danirljr.matirialdesign.ui.theme.MatirialDesignTheme
+import androidx.compose.ui.unit.dp
+import com.example.compose.AppTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MatirialDesignTheme {
+            AppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MatirialScreen()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MatirialScreen( modifier: Modifier = Modifier) {
+
+    val SliderState = remember { mutableStateOf(0.5f) }
+
+    Column {
+        TextField(value = "Input", onValueChange = {})
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Click me!!")
+        }
+
+
+        Switch(checked = true, onCheckedChange = {} )
+        Switch(checked = false, onCheckedChange = {} )
+        Slider(value = SliderState.value, onValueChange ={SliderState.value = it} )
+        RadioButton(selected = true, onClick = { /*TODO*/ })
+        RadioButton(selected = false, onClick = { /*TODO*/ })
+        CircularProgressIndicator(progress = SliderState.value)
+
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MatirialDesignTheme {
-        Greeting("Android")
+    AppTheme{
+        MatirialScreen()
     }
 }
